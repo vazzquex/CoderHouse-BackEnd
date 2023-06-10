975e1a58d28fe184ed4f150bf792be88d80e5291
Santiago Vazquez <vazquezgrillo.santiago@gmail.com>
2023-06-10 19:30:21 -0300

    uplolad pre entrega 2

---
+++ preEntrega-ProyectoFinal2/src/app.js
@@ -0,0 +1,59 @@
+// Dependencies
+import mongoose from 'mongoose';
+import express from 'express';
+import { Server } from 'socket.io';
+import handlebars from 'express-handlebars';
+import productManager from './dao/manager/ProductManager.js';
+// Routes
+
+import viewsRouter from './routes/views.router.js';
+import viewsCartsRouter from './routes/views.cart.router.js'
+import productsRouter from './routes/products.router.js';
+import realTimeProductsRouter from './routes/realTimeProducts.router.js';
+import chatRouter from './routes/chat.router.js';
+import cartsRouter from './routes/carts.router.js';
+// Config
+
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./src/public'));
+// Handlebars
+app.engine('handlebars', handlebars.engine());
+app.set('views', './src/views');
+app.set('view engine', 'handlebars');
+
+
+app.get('/', (req, res) => {
+    res.redirect('/products');
+})
+
+// api
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
+
+
+const MongoUrl = "mongodb://localhost:27017/ecommerce"
+
+mongoose.set("strictQuery", false);
+try {
+    await mongoose.connect("mongodb://localhost:27017/students");
+} catch {
+    console.error(`Database connection failed: ${error}`);
+};
+
+const httpServer = app.listen(port, () => {
+    console.log(`Escuchando por el puerto ${port}`);
+});
+const socketServer = new Server(httpServer);
+
+//Routers
+app.use("/products", viewsRouter);
+app.use("/carts", viewsCartsRouter);
+app.use('/realTimeProducts', realTimeProductsRouter(socketServer));
+app.use("/chat", chatRouter(socketServer));
+
+
+
---
+++ preEntrega-ProyectoFinal2/src/dao/manager/MessagesManager.js
@@ -0,0 +1,33 @@
+import messagesModel from "../models/messages.model.js";
+
+class MessagesManager {
+  getMessages = async () => {
+    try {
+      const messages = await messagesModel.find().lean().exec();
+      return messages;
+    } catch (error) {
+      console.error(`Error trying to get messages: ${error}`);
+    };
+  };
+
+  addMessage = async (msg) => {
+    try {
+      const user = msg.user.trim();
+      const message = msg.text.trim();
+      if(user.length < 1 || message.length < 1) return;
+      const formattedMessage = {
+        user: user,
+        message: message
+      };  
+      const newMessage = new messagesModel(formattedMessage);
+      await newMessage.save();
+      return newMessage;
+    } catch (error) {
+      console.error(`Error al enviar mensaje: ${error}`);
+    };
+  };
+};
+
+const messagesManager = new MessagesManager();
+
+export default messagesManager;
