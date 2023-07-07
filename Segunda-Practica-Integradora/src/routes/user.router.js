b17a1c23bf638618dea7585a56fe90fbeb582ae1
Santiago Vazquez <vazquezgrillo.santiago@gmail.com>
2023-07-07 19:22:50 -0300

    save cart

--- Segunda-Practica-Integradora/src/routes/user.router.js
+++ Segunda-Practica-Integradora/src/routes/user.router.js
@@ -26,37 +26,6 @@ const admin = {
 
 
 
-usersRouter.post('/:userId/cart', async (req, res) => {
-	const userId = req.params.userId;
-	const productId = req.body.productId;
-
-	console.log(userId);
-	res.render('user', { userId });
-
-  
-	try {
-	  // Buscar al usuario por su ID
-	  const user = await User.findById(userId);
-  
-	  if (!user) {
-		return res.status(404).json({ message: 'Usuario no encontrado' });
-	  }
-  
-	  // Añadir el producto al carrito del usuario
-	  user.cart.push(productId);
-  
-	  // Guardar los cambios en la base de datos
-	  await user.save();
-  
-	  return res.status(200).json({ message: 'Producto añadido al carrito correctamente' });
-	} catch (error) {
-	  console.error('Error al añadir producto al carrito:', error);
-	  return res.status(500).json({ message: 'Error interno del servidor' });
-	}
-  });
-  
-
-
 
 usersRouter.post('/auth', async (req, res) => {
 	const { email, password } = req.body;
