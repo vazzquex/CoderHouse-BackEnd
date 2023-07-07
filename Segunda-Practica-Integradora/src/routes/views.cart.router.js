2dd7af81464756ca1f8ea919f153a31c1fe9fb99
Santiago Vazquez <santiago@gmail.com>
2023-07-07 18:57:42 -0300

    car

--- Segunda-Practica-Integradora/src/routes/views.cart.router.js
+++ Segunda-Practica-Integradora/src/routes/views.cart.router.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 const router = Router();
 
+import productManager from '../dao/manager/ProductManager.js';
 import cartsManager from '../dao/manager/CartsManager.js';
 import userService from '../services/user.service.js';
 
@@ -8,14 +9,25 @@ import userService from '../services/user.service.js';
 router.post('/:userId', async (req, res) => {
     const { userId } = req.params;
     try {
-      const cart = await userService.getCartUser(userId);
-      res.status(200).json({ cart });
-      
+        const cart = await userService.getCartUser(userId);
+        const product = await productManager.getProducts();        
+        const { user } = req.session;
+        delete user.password;
+        
+        res.status(201).render('cart', {
+            title: "Cart",
+            cart,
+            product,
+            user
+        })
+
+        //res.status(200).json({ cart });
+
     } catch (error) {
-      console.error(`Error trying to get cart: ${error}`);
-      res.status(500).send(`Internal server error trying to get cart: ${error}`);
+        console.error(`Error trying to get cart: ${error}`);
+        res.status(500).send(`Internal server error trying to get cart: ${error}`);
     };
-  });
-  
+});
+
 
 export default router;
