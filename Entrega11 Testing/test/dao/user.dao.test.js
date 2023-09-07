68e81d575dbfebf8450da4066395d2bd0d1c99c5
Santiago Vazquez <vazquezgrillo.santiago@gmail.com>
2023-09-07 19:28:57 -0300

    add tests

---
+++ Entrega11 Testing/test/dao/user.dao.test.js
@@ -0,0 +1,67 @@
+import supertest from 'supertest';
+import chai from 'chai';
+import mongoose from 'mongoose';
+
+import config from '../../src/tools/config.js';
+
+import UserRepository from '../../src/repositories/user.repository.js';
+
+const expect = chai.expect;
+const request = supertest('http://localhost:8080');
+
+let userId;
+
+describe("Test User DAOs", () => {
+
+    before(async () => {
+        await mongoose.connect(config.mongoUrl)
+    })
+
+    after(async () => {
+        await mongoose.connection.close()
+    })
+
+    afterEach(async () => {
+        if (userId) {
+            try {
+                await mongoose.connection.collection('users').deleteOne({ _id: new mongoose.Types.ObjectId(userId) })
+            } catch (err){
+                console.error(`Error al eliminar el usuario ${err}`)
+            }
+
+        }
+    })
+
+    it("Create new user", async () => {
+
+        const newUser = {
+            first_name: 'Santiago',
+            last_name: 'Vazquez',
+            email: 'santiago@example.com',
+            password: '1234'
+        }
+
+        const {statusCode, _body} = await request.post('/api/users/').send(newUser);
+        
+
+        userId = _body._id;
+
+
+        expect(_body).to.have.property('_id')
+        expect(statusCode).to.equal(201)
+
+    })
+
+    it("The DAO must be able to obtain a user by email",async () => {
+
+
+
+    })
+
+
+
+
+
+
+})
+
