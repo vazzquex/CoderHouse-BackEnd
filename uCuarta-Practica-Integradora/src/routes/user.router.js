788682e0d13b72a8cec1ff4543a67dd6e2f857e0
Santiago Vazquez <vazquezgrillo.santiago@gmail.com>
2023-09-16 17:42:11 -0300

    update multer middleware

--- uCuarta-Practica-Integradora/src/routes/user.router.js
+++ uCuarta-Practica-Integradora/src/routes/user.router.js
@@ -1,35 +1,10 @@
 import { Router } from 'express';
-import multer from 'multer';
-
 import userController from '../controllers/user.controller.js';
-import path from 'path';
-import __dirname from '../utils.js';
 
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        let folder;
-
-        if (req.body.type === 'documents') {
-            folder = 'documents';
-        } else if (req.body.type === 'products') {
-            folder = 'products';
-        } else if (req.body.type === 'profiles') {
-            folder = 'profiles';
-        } else {
-            throw new Error('Invalid type for storage: ' + req.body.type);
-        }
-        cb(null, path.join(__dirname, `./data/${folder}`));
-    },
-    filename: (req, file, cb) => {
-        const timestamp = Date.now();
-        cb(null, `${timestamp}-${file.originalname}`);
-    },
-});
-
+//import multer middleware
+import { upload } from '../middleware/multer.middleware.js';
 
 const usersRouter = Router();
-const upload = multer({ storage });
 
 usersRouter.post('/', userController.createUser)
 usersRouter.post('/premium/:uid', userController.updateRol);
